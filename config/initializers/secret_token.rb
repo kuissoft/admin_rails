d658b23eeb83fb06b397f5584a2c56388419088d
Tomas Stanik <tomas.stanik@gmail.com>
2013-08-20 18:49:20 +0200

    Rails application created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RemoteAssistant::Application.config.secret_key_base = 'd065e25b022a4b03ef89d5762d9f5b9c9d08d266c143a8d33393381d24c7bd459f7567e5ab2d1aba2d8908c580604557d50a7618d7f1845aa8ac85ac22cccaff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RemoteAssistant::Application.config.session_store :cookie_store, key: '_RemoteAssistant_session'
