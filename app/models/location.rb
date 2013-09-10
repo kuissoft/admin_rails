2edab3ab75300c631ca3dddc6b7ce8c33841b3dd
Jakub Arnold <darthdeus@gmail.com>
2013-09-10 14:46:58 +0200

    Remove trailing whitespace

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,15 +1,15 @@
 class Location < ActiveRecord::Base
   belongs_to :session
-  
+
   validates :session,
     uniqueness: true
   validates :lat,
     inclusion: { in: -90..90, message: " is not a valid latitude" }
   validates :lon,
     inclusion: { in: -180..180, message: " is not a valid longitude" }
-  validates :bearing, 
-    inclusion: { in: 0..359, message: " must be within 0 to 359" }, 
+  validates :bearing,
+    inclusion: { in: 0..359, message: " must be within 0 to 359" },
     numericality: { only_integer: true }
-    
+
     scope :sorted, -> { order("created_at DESC") }
 end
