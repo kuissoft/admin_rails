300777b934a56919c30f6557a08cfc533a4c9834
Jakub Arnold <darthdeus@gmail.com>
2013-09-10 14:54:26 +0200

    Add user auth_token generation + tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ActiveRecord::Base
-  has_one :sender_session, 
-    class_name: "Session", 
-    inverse_of: :sender, 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  before_save :ensure_authentication_token
+
+  has_one :sender_session,
+    class_name: "Session",
+    inverse_of: :sender,
     foreign_key: "sender_id",
     dependent: :destroy
-  has_one :recipient_session, 
-    class_name: "Session", 
-    inverse_of: :recipient, 
+  has_one :recipient_session,
+    class_name: "Session",
+    inverse_of: :recipient,
     foreign_key: "recipient_id",
     dependent: :destroy
   has_one :location,
@@ -30,4 +37,19 @@ class User < ActiveRecord::Base
     :inclusion => 0..1
 
   scope :sorted, -> { order("role DESC, email ASC") }
+
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+
+  private
+
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.exists?(authentication_token: token)
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "new user automatically gets an authentication token" do
+    user = FactoryGirl.create(:user)
+    assert user.authentication_token.present?
+  end
+
+  test "user gets a new authenticaton token if he doesn't get one" do
+    user = FactoryGirl.create(:user)
+    user.authentication_token = nil
+    user.save!
+
+    assert user.authentication_token.present?
+  end
 end
