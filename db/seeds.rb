8ab296d926fdb621ec25d430cf77dbdecd0afd56
Jakub Arnold <darthdeus@gmail.com>
2013-09-25 17:02:56 +0200

    Change password validation to only require 5 characters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
 
+User.create!(name: 'Amin', phone: '12345', email: 'admin@example.com', password: 'password', role: 1)
 User.create!(name: 'Tomas Stanik', phone: '+421 917 328 431', email: 'tomas@remoteassistant.me', password: 'asdfasdf', role: 1)
 User.create!(name: 'Alok Sahay', phone: '+420 608 371 987', email: 'alok@remoteassistant.me', password: 'asdfasdf', role: 1)
 User.create!(name: 'Jana Filova', phone: '+420 608 166 448', email: 'jana@remoteassistant.me', password: 'asdfasdf', role: 1)
