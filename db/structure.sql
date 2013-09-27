ca79323e5b9e1e0df4fa9756282aac3f3c1d8640
Jakub Arnold <darthdeus@gmail.com>
2013-09-27 23:07:55 +0200

    Remove unused locations spec

--- db/structure.sql
+++ db/structure.sql
@@ -1,11 +1,8 @@
 CREATE TABLE "connections" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "user_id" integer, "contact_id" integer, "is_pending" boolean, "created_at" datetime, "updated_at" datetime, "is_rejected" boolean DEFAULT 'f');
 CREATE TABLE "locations" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "session_id" integer, "lat" float, "lon" float, "bearing" integer, "created_at" datetime, "updated_at" datetime);
 CREATE TABLE "schema_migrations" ("version" varchar(255) NOT NULL);
-CREATE TABLE "sessions" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "session_id" text, "sender_token" text, "recipient_token" text, "sender_id" integer, "recipient_id" integer, "created_at" datetime, "updated_at" datetime);
 CREATE TABLE "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(255), "phone" varchar(255), "email" varchar(255), "role" integer, "created_at" datetime, "updated_at" datetime, "encrypted_password" varchar(255) DEFAULT '' NOT NULL, "authentication_token" varchar(255), "reset_password_token" varchar(255), "reset_password_sent_at" datetime, "remember_created_at" datetime, "sign_in_count" integer DEFAULT 0 NOT NULL, "current_sign_in_at" datetime, "last_sign_in_at" datetime, "current_sign_in_ip" varchar(255), "last_sign_in_ip" varchar(255));
 CREATE INDEX "index_locations_on_session_id" ON "locations" ("session_id");
-CREATE INDEX "index_sessions_on_recipient_id" ON "sessions" ("recipient_id");
-CREATE INDEX "index_sessions_on_sender_id" ON "sessions" ("sender_id");
 CREATE UNIQUE INDEX "index_users_on_email" ON "users" ("email");
 CREATE UNIQUE INDEX "index_users_on_reset_password_token" ON "users" ("reset_password_token");
 CREATE UNIQUE INDEX "unique_schema_migrations" ON "schema_migrations" ("version");
@@ -20,3 +17,5 @@ INSERT INTO schema_migrations (version) VALUES ('20130910123149');
 INSERT INTO schema_migrations (version) VALUES ('20130920205148');
 
 INSERT INTO schema_migrations (version) VALUES ('20130926112229');
+
+INSERT INTO schema_migrations (version) VALUES ('20130927210520');
--- spec/requests/locations_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "Locations" do
-  it "requires an authenticaton token" do
-    get locations_path(format: :json)
-    response.status.should == 401
-  end
-end
