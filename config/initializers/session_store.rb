dba7ff8c6765dca6d86564f84bc942b30d5fafe6
Jakub Arnold <darthdeus@gmail.com>
2013-10-17 11:12:47 +0200

    Switch to dalli_store for cache and sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-RemoteAssistant::Application.config.session_store :cookie_store, key: '_RemoteAssistant_session'
+RemoteAssistant::Application.config.session_store :dalli_store, key: '_RemoteAssistant_session'
