ab67b8903770294c20531921642ec9fe271513b3
Jakub Arnold <darthdeus@gmail.com>
2013-10-17 10:22:21 +0200

    Make node api url configurable

--- app/services/realtime.rb
+++ app/services/realtime.rb
@@ -3,7 +3,11 @@ class Realtime
     @config = RockConfig.for "sockets"
   end
 
+  def host
+    ENV["API_NODE_URL"] || @config.host
+  end
+
   def notify(user_id, event, data)
-    RestClient.post(@config.host + "/notify", { user_id: user_id, event: event, data: data })
+    RestClient.post(host + "/notify", { user_id: user_id, event: event, data: data })
   end
 end
