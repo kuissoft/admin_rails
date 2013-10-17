f4efb0bccb6008b989f0467cd181449ea272b97b
Jakub Arnold <darthdeus@gmail.com>
2013-10-17 11:17:24 +0200

    Fix dalli session store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
+require 'action_dispatch/middleware/session/dalli_store'
 RemoteAssistant::Application.config.session_store :dalli_store, key: '_RemoteAssistant_session'
