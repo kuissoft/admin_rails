8a3a706c042ed32f140f384288d8c5758ceb13c0
Jakub Arnold <darthdeus@gmail.com>
2013-10-17 10:17:14 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 User.destroy_all
 
-User.create!(name: 'Amin', phone: '12345', email: 'admin@example.com', password: 'password', role: 1)
 User.create!(name: 'Tomas Stanik', phone: '+421 917 328 431', email: 'tomas@remoteassistant.me', password: 'asdfasdf', role: 1)
-User.create!(name: 'Alok Sahay', phone: '+420 608 371 987', email: 'alok@remoteassistant.me', password: 'asdfasdf', role: 1)
+User.create!(name: 'Jakub Arnold', phone: '+420 774 595 676', email: 'jakub@remoteassistant.me', password: 'asdfasdf', role: 1)
 User.create!(name: 'Jana Filova', phone: '+420 608 166 448', email: 'jana@remoteassistant.me', password: 'asdfasdf', role: 1)
