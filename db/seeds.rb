8abc117f8fd7f8f805c902340ae7c8183b0eb096
Jakub Arnold <darthdeus@gmail.com>
2013-10-17 10:34:35 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,22 @@
 User.destroy_all
 
-User.create!(name: 'Tomas Stanik', phone: '+420 606 484 899', email: 'tomas@remoteassistant.me', password: 'asdfasdf', role: 1)
-User.create!(name: 'Tomas Stanik', phone: '+421 917 328 431', email: 'tomas.stanik@gmail.com', password: 'asdfasdf', role: 1)
-User.create!(name: 'Jakub Arnold', phone: '+420 774 595 676', email: 'jakub@remoteassistant.me', password: 'asdfasdf', role: 1)
-User.create!(name: 'Jana Filova',  phone: '+420 608 166 448', email: 'jana@remoteassistant.me', password: 'asdfasdf', role: 1)
+u1 = User.create!(name: 'Tomas Stanik', phone: '+420 606 484 899', email: 'tomas@remoteassistant.me', password: 'asdfasdf', role: 1)
+u2 = User.create!(name: 'Tomas Stanik', phone: '+421 917 328 431', email: 'tomas.stanik@gmail.com', password: 'asdfasdf', role: 1)
+u3 = User.create!(name: 'Jakub Arnold', phone: '+420 774 595 676', email: 'jakub@remoteassistant.me', password: 'asdfasdf', role: 1)
+u4 = User.create!(name: 'Jana Filova',  phone: '+420 608 166 448', email: 'jana@remoteassistant.me', password: 'asdfasdf', role: 1)
+
+Connection.create!(user: u1, connection: u2, is_pending: false)
+Connection.create!(user: u1, connection: u3, is_pending: false)
+Connection.create!(user: u1, connection: u4, is_pending: false)
+
+Connection.create!(user: u2, connection: u1, is_pending: false)
+Connection.create!(user: u2, connection: u3, is_pending: false)
+Connection.create!(user: u2, connection: u4, is_pending: false)
+
+Connection.create!(user: u3, connection: u1, is_pending: false)
+Connection.create!(user: u3, connection: u2, is_pending: false)
+Connection.create!(user: u3, connection: u4, is_pending: false)
+
+Connection.create!(user: u4, connection: u1, is_pending: false)
+Connection.create!(user: u4, connection: u2, is_pending: false)
+Connection.create!(user: u4, connection: u3, is_pending: false)
