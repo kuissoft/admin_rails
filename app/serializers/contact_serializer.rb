f854d5dbef33bc6649afa4229d83a40fee0f06b0
Jiří Kratochvíl <info@sedrick.cz>
2014-01-09 13:21:01 +0100

    + Fix contact serializer error

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,18 +2,18 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :phone, :is_pending, :is_rejected, :is_removed
 
   def id
-    object.contact.id
+    object.contact.id if object.contact
   end
 
   def name
-    object.contact.name
+    object.contact.name if object.contact
   end
 
   def email
-    object.contact.email
+    object.contact.email if object.contact
   end
 
   def phone
-    object.contact.phone
+    object.contact.phone if object.contact
   end
 end
