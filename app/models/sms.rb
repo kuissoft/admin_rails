71e2364e8fa925b9c8f4fcb27a2efba5285907c4
Jiří Kratochvíl <info@sedrick.cz>
2014-02-12 20:08:30 +0100

    + Remove phone hack

--- app/models/sms.rb
+++ app/models/sms.rb
@@ -5,18 +5,16 @@ class Sms
   attr_accessor :send_to, :msg, :send_from
 
   def initialize send_to, msg
-    @send_to = who_to_send send_to
+    @send_to = send_to
     @msg = msg
-    @send_from = "+15736147427"
+    @send_from = get_settings_value(:twillio_sms_number) 
   end
 
 
   def deliver
     # put your own credentials here
-    # account_sid = 'ACad62729938ab760ec8e723e090553dbf'
-    account_sid = 'AC32d59eb259a02e9b1f4b88f791cace9a'
-    # auth_token = 'b94b4dfaec50c47a8acda1b8151db75a'
-    auth_token = '58c70a50ec898f4fa32a00f2514a3ea1'
+    account_sid = get_settings_value(:twillio_account_sid) 
+    auth_token = get_settings_value(:twillio_auth_token) 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new account_sid, auth_token
 
@@ -36,14 +34,6 @@ class Sms
       Rails.logger.error "Twilio REST Error:  #{e.message}"
       Rails.logger.info "================ DEBUG TWILIO END ================="
       return [false, "Cannot send validation SMS. Please try again later."]
-    end
-  end
-
-  def who_to_send send_to
-    if send_to[1..2] == get_settings_value(:dev_prefix, "99")
-      return get_settings_value(:dev_phones)[send_to[3].to_i]
-    else
-      return send_to
     end
   end
 
