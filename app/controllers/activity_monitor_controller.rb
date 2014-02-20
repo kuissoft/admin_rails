054bf3d325e566301b84dd70b3ac1bd36a9595ce
Jiří Kratochvíl <info@sedrick.cz>
2014-02-20 10:59:46 +0100

    + Add error handling if Node server is offline to Activity monitor

--- app/controllers/activity_monitor_controller.rb
+++ app/controllers/activity_monitor_controller.rb
@@ -2,12 +2,20 @@ class ActivityMonitorController < AuthenticatedController
   require 'open-uri'
   def index
     @logs = []
-    open("#{NODE_HOST}/development.log", :http_basic_authentication=>[NODE_ACCESS_NAME, NODE_ACCESS_PASSWORD]).each_line{|line| @logs <<  ActiveSupport::JSON.decode(line) rescue ""}
+    begin
+      open("#{NODE_HOST}/development.log", :http_basic_authentication=>[NODE_ACCESS_NAME, NODE_ACCESS_PASSWORD]).each_line{|line| @logs <<  ActiveSupport::JSON.decode(line) rescue ""}
+    rescue
+      @logs << {'level' => "info", 'message' => 'NODE SERVER IS OFFLINE', 'timestamp' => Time.now}
+    end
   end
 
   def refresh_logs
     @logs = []
-    open("#{NODE_HOST}/development.log", :http_basic_authentication=>[NODE_ACCESS_NAME, NODE_ACCESS_PASSWORD]).each_line{|line| @logs <<  ActiveSupport::JSON.decode(line) rescue ""}
+    begin
+      open("#{NODE_HOST}/development.log", :http_basic_authentication=>[NODE_ACCESS_NAME, NODE_ACCESS_PASSWORD]).each_line{|line| @logs <<  ActiveSupport::JSON.decode(line) rescue ""}
+    rescue
+      @logs << {'level' => "info", 'message' => 'NODE SERVER IS OFFLINE', 'timestamp' => Time.now}
+    end
     respond_to do |format|
       format.js
     end
