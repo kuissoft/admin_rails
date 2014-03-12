d7c7284dacc319d0fab2566bb148520bf4ab81df
pdostal <git@pdostal.cz>
2014-03-12 14:19:29 +0100

    Add environment variable

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -2,6 +2,7 @@ role :app, %w{ubuntu@dev.remoteassistant.me}
 role :web, %w{ubuntu@dev.remoteassistant.me}
 role :db,  %w{ubuntu@dev.remoteassistant.me}
 
+set :rails_env, "production"
 set :stage, 'development'
 set :application, 'web'
 set :repo_url, 'git@bitbucket.org:remoteassistant/rails.git'
