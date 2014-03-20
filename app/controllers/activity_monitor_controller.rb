45af9bc41e0f7270d2d1bfa4e85a5295a97212c5
Jiří Kratochvíl <info@sedrick.cz>
2014-03-20 09:20:16 +0100

    + Add devices to activity monitor

--- app/controllers/activity_monitor_controller.rb
+++ app/controllers/activity_monitor_controller.rb
@@ -3,7 +3,7 @@ class ActivityMonitorController < AuthenticatedController
   def index
     @logs = []
     begin
-      open("#{NODE_HOST}/development.log", :http_basic_authentication=>[NODE_ACCESS_NAME, NODE_ACCESS_PASSWORD]).each_line{|line| @logs <<  ActiveSupport::JSON.decode(line) rescue ""}
+      open("#{NODE_HOST}/log", :http_basic_authentication=>[NODE_ACCESS_NAME, NODE_ACCESS_PASSWORD]).each_line{|line| @logs <<  ActiveSupport::JSON.decode(line) rescue ""}
     rescue
       @logs << {'level' => "info", 'message' => 'NODE SERVER IS OFFLINE', 'timestamp' => Time.now}
     end
@@ -12,7 +12,7 @@ class ActivityMonitorController < AuthenticatedController
   def refresh_logs
     @logs = []
     begin
-      open("#{NODE_HOST}/development.log", :http_basic_authentication=>[NODE_ACCESS_NAME, NODE_ACCESS_PASSWORD]).each_line{|line| @logs <<  ActiveSupport::JSON.decode(line) rescue ""}
+      open("#{NODE_HOST}/log", :http_basic_authentication=>[NODE_ACCESS_NAME, NODE_ACCESS_PASSWORD]).each_line{|line| @logs <<  ActiveSupport::JSON.decode(line) rescue ""}
     rescue
       @logs << {'level' => "info", 'message' => 'NODE SERVER IS OFFLINE', 'timestamp' => Time.now}
     end
