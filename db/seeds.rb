030e3b41f12795b1dc0f11d012df64ae2f8cd60b
Jiří Kratochvíl <info@sedrick.cz>
2014-03-20 15:21:24 +0100

    + Add Jojo to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ User.create!(name:'Jiří Urbášek', phone:'+420604156985', email:'jirka@remote
 User.create!(name:'Jiří Urbášek 2', phone:'+420723385838', email:'jiri.urbasek@gmail.com', password:'admin', role:'admin')
 User.create!(name:'Pavel Dostál', phone:'+420773646660', email:'pavel@remoteassistant.me', password:'admin', role:'admin')
 User.create!(name:'Pavel Dostál 2', phone:'+4408703976885', email:'pdostal@pdostal.cz', password:'admin', role:'admin')
+User.create!(name:'Jozef Matúš', phone:'+420728408356', email:'jozef@remoteassistant.me', password:'admin', role:'admin')
+User.create!(name:'Jozef Matúš 2', phone:'+421911912352', email:'jojo@matus.sk', password:'admin', role:'admin')
 
 
 # SETTINGS SEED
@@ -22,4 +24,5 @@ Setting.create!([{ name: 'token_expiration_period', value: '300' },
  { name: 'twillio_sms_number', value: '+15736147427' },
  { name: 'twillio_account_sid', value: 'AC32d59eb259a02e9b1f4b88f791cace9a' },
  { name: 'twillio_auth_token', value: '58c70a50ec898f4fa32a00f2514a3ea1' },
+ { name: 'force_sms', value: '0' },
  ])
