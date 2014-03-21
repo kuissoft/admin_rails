e48e26a5859113cc081a0d96f8e046764fce8473
pdostal <git@pdostal.cz>
2014-03-21 13:00:15 +0100

    Update datetime in devices table

--- config/initializers/formatter.rb
+++ config/initializers/formatter.rb
@@ -1,5 +1,5 @@
 #Time::DATE_FORMATS[:default] = "%d.%m.%Y at %H:%M:%S"
 #Date::DATE_FORMATS[:default] = "%d.%m.%Y"
-Time::DATE_FORMATS[:datetime] = "%d.%m.%Y at %H:%M"
+Time::DATE_FORMATS[:datetime] = "%d.%m.%Y %H:%M:%S"
 Time::DATE_FORMATS[:default] = "%H:%M"
 Date::DATE_FORMATS[:default] = "%d.%m.%Y"
