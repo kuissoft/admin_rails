981cb6088a64b021099e5e74d11f2863a8d4bf31
pdostal <git@pdostal.cz>
2014-04-07 18:30:34 +0200

    Refactor authentication API test

--- spec/factories/authentication.rb
+++ spec/factories/authentication.rb
@@ -1,9 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :authenticate do
+  factory :authentication do
   	phone "420123456789"
-    uuid "3s2d4fd2f4fd2"
-    language "en"
   end
 end
