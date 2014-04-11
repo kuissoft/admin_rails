6bdaa034ef3c223ef177e0cd53e18814bc182b35
pdostal <git@pdostal.cz>
2014-04-11 11:22:30 +0200

    Change iOS feedback from BaseCamp to Trello and add first 10 words to subject

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -3,10 +3,10 @@ class Emailer < ActionMailer::Base
 
   def feedback_email feedback 
     @feedback = feedback
-
     @user = User.where(id: @feedback.user_id ).first if @feedback.user_id
+    submess = /((^| )[^ ]+){10}/.match(@feedback.message)
 
-    mail to: "project-4944826-c6516e79fe6f8eada19a7dcf@basecamp.com", subject: 'Feedback (iOS)'
+    mail to: "tomasstanik+k81yzne7dckwyluqnmpj@boards.trello.com", subject: "iOS: #{submess}"
   end
 
   def authentication_email user, device
