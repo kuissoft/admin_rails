352a72fa175aceccdd6a18e0ce505a9c5bd5e59b
Jiří Kratochvíl <jiri@remoteassistant.me>
2014-04-16 18:35:36 +0000

    + Comment Silenceble logger

--- config/initializers/silencable_logger.rb
+++ config/initializers/silencable_logger.rb
@@ -1,29 +1,29 @@
-NullLogger = Logger.new('/dev/null')
+# NullLogger = Logger.new('/dev/null')
 
-Rails::Rack::Logger.class_eval do
-  def call_with_silenced_logger(env = {})
-    silence = (
-      env['QUERY_STRING'] =~ %r[silence=logger]iomx ||
-      env['PATH_INFO'] =~ %r[(^/assets\b)|(^/su/logs\b)]iomx
-    )
+# Rails::Rack::Logger.class_eval do
+#   def call_with_silenced_logger(env = {})
+#     silence = (
+#       env['QUERY_STRING'] =~ %r[silence=logger]iomx ||
+#       env['PATH_INFO'] =~ %r[(^/assets\b)|(^/su/logs\b)]iomx
+#     )
 
-    if silence
-      begin
-        a = Rails.logger
-        b = ActionController::Base.logger
-        Rails.logger = NullLogger
-        ActionController::Base.logger =  NullLogger
-        call_without_silenced_logger(env)
-      ensure
-        Rails.logger = a
-        ActionController::Base.logger = b
-      end
-    else
-      call_without_silenced_logger(env)
-    end
-  end
+#     if silence
+#       begin
+#         a = Rails.logger
+#         b = ActionController::Base.logger
+#         Rails.logger = NullLogger
+#         ActionController::Base.logger =  NullLogger
+#         call_without_silenced_logger(env)
+#       ensure
+#         Rails.logger = a
+#         ActionController::Base.logger = b
+#       end
+#     else
+#       call_without_silenced_logger(env)
+#     end
+#   end
 
-  alias_method_chain(:call, :silenced_logger)
-end
+#   alias_method_chain(:call, :silenced_logger)
+# end
 
-Rails.application.assets.logger = NullLogger
+# Rails.application.assets.logger = NullLogger
