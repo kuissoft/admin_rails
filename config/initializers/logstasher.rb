45283957819f04f6df14c1d02166cc2a566768cb
pdostal <git@pdostal.cz>
2014-04-29 17:57:01 +0200

    Move important code from simple_logger.rb

--- config/initializers/logstasher.rb
+++ config/initializers/logstasher.rb
@@ -7,14 +7,37 @@ if LogStasher.enabled
 end
 
 class Rails::Rack::Logger
- def call_app(*args)
-   puts "Hello: #{args.inspect}"
-   env = args.last
-   @app.call(env)
- ensure
-   ActiveSupport::LogSubscriber.flush_all!
- end
+  def call_app(*args)
+    puts "Hello: #{args.inspect}"
+    env = args.last
+    @app.call(env)
+  ensure
+    ActiveSupport::LogSubscriber.flush_all!
+  end
 
- def before_dispatch(env)
- end
+  def before_dispatch(env)
+  end
+
+  def log_to_node(time, severity, msg)
+    @@cnt += 1
+    request = RestClient::Request.new(
+      method: :post,
+      url: NODE_HOST + "/log_from_rails",
+      user: 'remote',
+      password: 'asdfasdf',
+      payload: {
+        time: time,
+        severity: severity,
+        msg: msg
+    })
+    begin
+      if @@cnt == 2
+        @@cnt = 0
+      else
+        request.execute unless msg.empty?
+      end
+    rescue => e
+      puts "rescued"
+    end
+  end
 end
