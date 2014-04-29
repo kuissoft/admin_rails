28111b1563fb149c1822e08f9f9cd8d2163e2127
pdostal <git@pdostal.cz>
2014-04-29 17:50:58 +0200

    Remove comments

--- config/initializers/logstasher.rb
+++ config/initializers/logstasher.rb
@@ -1,18 +1,12 @@
 if LogStasher.enabled
   LogStasher.add_custom_fields do |fields|
-    # This block is run in application_controller context,
-    # so you have access to all controller methods
     fields[:user] = current_user && current_user.phone
     fields[:site] = request.path =~ /^\/api/ ? 'api' : 'admin'
     #fields.except!(:route)
-
-    # If you are using custom instrumentation, just add it to logstasher custom fields
-    #LogStasher.custom_fields << :myapi_runtime
   end
 end
 
 class Rails::Rack::Logger
- # Overwrites Rails 3.2 code that logs new requests
  def call_app(*args)
    puts "Hello: #{args.inspect}"
    env = args.last
@@ -21,7 +15,6 @@ class Rails::Rack::Logger
    ActiveSupport::LogSubscriber.flush_all!
  end
 
- # Overwrites Rails 3.0/3.1 code that logs new requests
  def before_dispatch(env)
  end
 end
