2f2b15875fb7bd05726e938af34a91bf59174bf2
pdostal <git@pdostal.cz>
2014-05-20 10:31:15 +0200

    Fix viewname for empty connections

--- app/helpers/connections_helper.rb
+++ app/helpers/connections_helper.rb
@@ -3,16 +3,16 @@ module ConnectionsHelper
     if user.name.blank?
       if contact.blank?
         conns = Connection.where(contact_id: user.id)
+        conn = conns.last
         conns.each do |c|
           conn = c if c.nickname.present?
         end
-        conn = conns.last
       else
         conns = Connection.where(contact_id: user.id, user_id: contact.id)
+        conn = conns.last
         conns.each do |c|
           conn = c if c.nickname.present?
         end
-        conn = conns.last
       end
       if conn.present?
         unless conn.nickname.blank?
