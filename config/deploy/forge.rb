e49973c4f270d496eea02dc81591d58753ffd582
Jiří Kratochvíl <jiri@remoteassistant.me>
2014-05-21 16:08:33 +0000

    + Remove env forge

--- config/deploy/forge.rb
+++ config/deploy/forge.rb
@@ -2,7 +2,7 @@ role :app, %w{web@sedrick.cz}
 role :web, %w{web@sedrick.cz}
 role :db,  %w{web@sedrick.cz}
 
-set :rails_env, "forge"
+set :rails_env, "production"
 set :stage, 'forge'
 set :application, 'web'
 set :repo_url, 'git@bitbucket.org:remoteassistant/rails.git'
