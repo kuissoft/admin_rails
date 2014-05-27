ee85ae7d81d5c804e793b40acdb6cdbdc9222bc8
pdostal <git@pdostal.cz>
2014-05-27 14:45:00 +0200

    Remove API

--- app/controllers/api/v1/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::ApplicationController < ActionController::Base
-  include ApplicationHelper
-  def set_language_by_area_code phone
-    lang = 'en'
-    lang = 'cs' if phone[1..3] == "420"
-    lang = 'sk' if phone[1..3] == "421"
-    lang
-  end
-end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  
+
   skip_before_filter :verify_authenticity_token
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -8,31 +8,4 @@ class ApplicationController < ActionController::Base
   include SharedControllerMethods
 
 
-  
-
-  # CUSTOM EXCEPTION HANDLING
-  rescue_from Exception do |e|
-    error(e)
-  end
- 
-  def routing_error
-    raise ActionController::RoutingError.new(params[:path])
-  end
- 
-  protected
-  
-  def error(e)
-    #render :template => "#{Rails::root}/public/404.html"
-    if env["ORIGINAL_FULLPATH"] =~ /^\/api/
-      logger.error "Routing Error: #{e.inspect}"
-
-    render json: { error_info: { code: 113, title: '', message: t('errors.url_or_record_not_found')} }, status: 500
-    else
-      logger.error "Routing Error: #{e.inspect}"
-      raise e
-      render json: { error_info: { code: 100, title: '', message: t('errors.undefined_error')} }, status: 500
-      #render :text => "500 Internal Server Error", :status => 500 # You can render your own template here
-    end
-  end
-  
 end
