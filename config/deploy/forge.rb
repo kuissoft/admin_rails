565f765c1da26f26c425da46f1bfc04ca3e46033
pdostal <git@pdostal.cz>
2014-05-27 15:18:15 +0200

    Update capistrano

--- config/deploy/forge.rb
+++ config/deploy/forge.rb
@@ -5,9 +5,9 @@ role :db,  %w{web@sedrick.cz}
 set :rails_env, "production"
 set :stage, 'forge'
 set :application, 'web'
-set :repo_url, 'git@bitbucket.org:remoteassistant/rails.git'
+set :repo_url, 'git@bitbucket.org:remoteassistant/admin.git'
 
 set :branch, "forge"
-set :deploy_to, '/home/web/ruby/rea'
+set :deploy_to, '/home/web/ruby/admin'
 
 server 'sedrick.cz', user: 'web', roles: %w{web app}
