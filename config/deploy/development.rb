890378b02e71a678bfdc18a899e7a00a0708a8f5
Jiří Kratochvíl <jiri@remoteassistant.me>
2014-06-02 14:17:23 +0000

    + Devide admin api

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -5,9 +5,9 @@ role :db,  %w{ubuntu@dev.remoteassistant.me}
 set :rails_env, "production"
 set :stage, 'development'
 set :application, 'web'
-set :repo_url, 'git@bitbucket.org:remoteassistant/rails.git'
+set :repo_url, 'git@bitbucket.org:remoteassistant/admin.git'
 
 set :branch, "development"
-set :deploy_to, '/home/ubuntu/rails'
+set :deploy_to, '/home/ubuntu/admin'
 
 server 'dev.remoteassistant.me', user: 'ubuntu', roles: %w{web app}
