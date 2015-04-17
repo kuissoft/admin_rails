72bbf12b2fd18aea33c1dc45e0f8a1312358d1ec
Tomas Stanik <tomas.stanik@gmail.com>
2015-04-17 15:52:00 +0200

    Change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,9 @@ ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY;")
 
 # puts 'Seed admin users'
 User.create!(name:'Tomáš Staník', phone:'+420606484899', email:'tomas@remoteassistant.me', password:'admin', role:'admin')
-User.create!(name:'Tomáš Staník 2', phone:'+421917328431', email:'tomas.stanik@gmail.com', password:'admin', role:'admin')
-User.create!(name:'Jiří Kratochvíl', phone:'+420720733688', email:'jiri@remoteassistant.me', password:'admin', role:'admin')
-User.create!(name:'Jiří Kratochvíl 2', phone:'+420602302314', email:'kratoj@gmail.com', password:'admin', role:'admin')
-User.create!(name:'Jiří Urbášek', phone:'+420604156985', email:'jirka@remoteassistant.me', password:'admin', role:'admin')
-User.create!(name:'Jiří Urbášek 2', phone:'+420723385838', email:'jiri.urbasek@gmail.com', password:'admin', role:'admin')
-User.create!(name:'Pavel Dostál', phone:'+420773646660', email:'pavel@remoteassistant.me', password:'admin', role:'admin')
-User.create!(name:'Pavel Dostál 2', phone:'+4408703976885', email:'pdostal@pdostal.cz', password:'admin', role:'admin')
+User.create!(name:'Tomáš Staník (Operátor)', phone:'+421917328431', email:'tomas.stanik@gmail.com', password:'admin', role:'operator')
+User.create!(name:'Róbert Tarabčák', phone:'+421902310128', email:'tarabcakr@gmail.com', password:'admin', role:'admin')
+User.create!(name:'Róbert Tarabčák (Operátor)', email:'tarabcak.r@gmail.com', password:'admin', role:'operator')
 
 User.all.each{|u| u.update password: "admin"}
 
