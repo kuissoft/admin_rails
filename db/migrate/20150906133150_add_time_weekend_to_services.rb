a9c1228e277ae35df4b4840da61fb29649b2c845
Tomas Stanik <tomas.stanik@gmail.com>
2015-09-06 18:25:54 +0200

    Uncomment migration

--- db/migrate/20150906133150_add_time_weekend_to_services.rb
+++ db/migrate/20150906133150_add_time_weekend_to_services.rb
@@ -1,6 +1,6 @@
 class AddTimeWeekendToServices < ActiveRecord::Migration
   def change
-    # add_column :services, :time_from_weekend, :time
-    # add_column :services, :time_until_weekend, :time
+    add_column :services, :time_from_weekend, :time
+    add_column :services, :time_until_weekend, :time
   end
 end
